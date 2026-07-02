36b170a2192d7e4ce2a71d26a11eb6c04a8ba782
nikhil-chn07 <145166964+nikhil-chn07@users.noreply.github.com>
2026-07-03 03:48:09 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,13 +1,8 @@
 import json
 import requests
 
-# Load products
 with open("products.json", "r", encoding="utf-8") as f:
     products = json.load(f)["products"]
-
-print("=" * 40)
-print("Universal Stock Tracker")
-print("=" * 40)
 
 headers = {
     "User-Agent": "Mozilla/5.0"
@@ -15,23 +10,17 @@ headers = {
 
 for product in products:
 
-    print("\n------------------------------")
-    print("Product:", product["name"])
+    print("=" * 50)
+    print(product["name"])
 
-    try:
-        response = requests.get(
-            product["url"],
-            headers=headers,
-            timeout=30
-        )
+    response = requests.get(
+        product["url"],
+        headers=headers,
+        timeout=30
+    )
 
-        print("Status Code:", response.status_code)
+    print("Status:", response.status_code)
+    print("Length:", len(response.text))
 
-        if response.status_code == 200:
-            print("✅ Downloaded Successfully")
-            print("Page Length:", len(response.text))
-        else:
-            print("❌ Failed")
-
-    except Exception as e:
-        print("Error:", e)
+    print("\nFIRST 1000 CHARACTERS:\n")
+    print(response.text[:1000])
