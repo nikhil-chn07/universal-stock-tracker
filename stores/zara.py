ca3db06e4b24fe6f5195e984e58cd526f6795422
nikhil-chn07 <145166964+nikhil-chn07@users.noreply.github.com>
2026-07-04 00:23:01 +0530

    Update zara.py

--- stores/zara.py
+++ stores/zara.py
@@ -1,7 +1,28 @@
+import requests
+
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/138.0 Safari/537.36"
+    )
+}
+
+
 def check(product):
+
     print("Checking Zara...")
-    print(product["url"])
+
+    response = requests.get(
+        product["url"],
+        headers=HEADERS,
+        timeout=30
+    )
+
+    print("Status Code :", response.status_code)
+    print("Page Length :", len(response.text))
 
     return {
-        "status": "Not Implemented"
+        "status": response.status_code,
+        "length": len(response.text)
     }
