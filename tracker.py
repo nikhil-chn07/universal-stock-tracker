ef60642e85a6fa1fc01e8bed069a15eb346fe0d5
nikhil-chn07 <145166964+nikhil-chn07@users.noreply.github.com>
2026-07-04 00:21:15 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,4 +1,5 @@
 import json
+from stores import zara
 
 with open("products.json", "r", encoding="utf-8") as f:
     products = json.load(f)["products"]
@@ -7,10 +8,11 @@ print("=" * 40)
 print("Universal Stock Tracker")
 print("=" * 40)
 
-print(f"Loaded {len(products)} products\n")
-
 for product in products:
-    print("Product :", product["name"])
-    print("URL     :", product["url"])
-    print("Sizes   :", ", ".join(product["sizes"]))
-    print("-" * 40)
+
+    if "zara.com" in product["url"]:
+        result = zara.check(product)
+        print(result)
+
+    else:
+        print("Store not supported")
