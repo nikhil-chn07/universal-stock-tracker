2310340fc862e5f6444e5804e92d0322859ee16b
nikhil-chn07 <145166964+nikhil-chn07@users.noreply.github.com>
2026-07-04 00:42:34 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,8 +1,16 @@
 import json
 from stores import zara
 
+# Load products
 with open("products.json", "r", encoding="utf-8") as f:
     products = json.load(f)["products"]
+
+# Load previous state
+try:
+    with open("state.json", "r", encoding="utf-8") as f:
+        state = json.load(f)
+except:
+    state = {}
 
 print("=" * 40)
 print("Universal Stock Tracker")
@@ -10,9 +18,24 @@ print("=" * 40)
 
 for product in products:
 
-    if "zara.com" in product["url"]:
-        result = zara.check(product)
-        print(result)
+    url = product["url"]
 
+    if "zara.com" in url:
+        result = zara.check(product)
     else:
-        print("Store not supported")
+        continue
+
+    current = result["status"]
+    previous = state.get(url)
+
+    print(f"Previous: {previous}")
+    print(f"Current : {current}")
+
+    if previous != current:
+        print("🔔 STOCK STATUS CHANGED!")
+
+    state[url] = current
+
+# Save latest state
+with open("state.json", "w", encoding="utf-8") as f:
+    json.dump(state, f, indent=4)
